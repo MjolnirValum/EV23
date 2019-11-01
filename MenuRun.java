3cebad18da2e87f2196f553ec5a7d671c059aba6
MjolnirValum <clayton.c.johnsen@gmail.com>
2019-11-01 05:01:40 -0500

    Add files via upload
    Added the current JAVA files, the JAR file to execute the code, and a ZIP file that contains everything.
    The ZIP was is a one-click to make it easier, once downloaded and worked on the JAVA files can be merged individually.

---
+++ MenuRun.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package edu.wiu.cs491;
+
+/**
+ *
+ * @author asche
+ */
+public class MenuRun {
+    public static void main(String[] args){
+        //do stuff here
+        MainMenuFrame mainMenuFrame = new MainMenuFrame();
+        mainMenuFrame.setVisible(true);
+    }
+    
+
+}
